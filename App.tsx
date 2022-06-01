d441f2f345057a9da1de02ec729520b9bf71c730
marcwright <marc.wright@gmail.com>
2022-05-31 19:44:19 -0500

    2 files modified

--- App.tsx
+++ App.tsx
@@ -7,13 +7,14 @@ import * as React from 'react';
 import './style.css';
 
 import { AgGridReact } from 'ag-grid-react';
-import { DataGrid } from '@mui/x-data-grid';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 import { useState, useEffect, useMemo, useCallback, useRef } from 'react';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import Button from '@mui/material/Button';
+import BalanceSharpIcon from '@mui/icons-material/BalanceSharp';
 
 export default function App() {
   const gridRef = useRef(); // Optional - for accessing Grid's API
@@ -53,8 +54,93 @@ export default function App() {
     gridRef.current.api.deselectAll();
   }, []);
 
+  // const [socketUrl, setSocketUrl] = useState(
+  //   'wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self'
+  // );
+  // const [messageHistory, setMessageHistory] = useState([]);
+
+  // const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+
+  // useEffect(() => {
+  //   if (lastMessage !== null) {
+  //     setMessageHistory((prev) => prev.concat(lastMessage));
+  //   }
+  // }, [lastMessage, setMessageHistory]);
+
+  // const handleClickChangeSocketUrl = useCallback(
+  //   () => setSocketUrl('wss://demos.kaazing.com/echo'),
+  //   []
+  // );
+
+  // const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
+
+  // const connectionStatus = {
+  //   [ReadyState.CONNECTING]: 'Connecting',
+  //   [ReadyState.OPEN]: 'Open',
+  //   [ReadyState.CLOSING]: 'Closing',
+  //   [ReadyState.CLOSED]: 'Closed',
+  //   [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  // }[readyState];
+
+  // let socket = new WebSocket(
+  //   'wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self'
+  // );
+
+  let socket = new WebSocket('wss://ws.bitstamp.net');
+  const subscribeMsg = {
+    event: 'bts:subscribe',
+    data: {
+      channel: 'order_book_btcusd',
+    },
+  };
+  socket.onopen = function (e) {
+    console.log('[open] Connection established');
+    console.log('Sending to server');
+    socket.send(JSON.stringify(subscribeMsg));
+  };
+
+  socket.onmessage = function (event) {
+    // console.log(`[message] Data received from server: ${event.data}`);
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      console.log(
+        `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
+      );
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      console.log('[close] Connection died');
+    }
+  };
+
+  socket.onerror = function (error) {
+    console.log(`[error] ${error.message}`);
+  };
+
   return (
     <div className="ag-theme-alpine" style={{ height: 500 }}>
+      <BalanceSharpIcon />
+      {/* <div>
+        <button onClick={handleClickChangeSocketUrl}>
+          Click Me to change Socket Url
+        </button>
+        <button
+          onClick={handleClickSendMessage}
+          disabled={readyState !== ReadyState.OPEN}
+        >
+          Click Me to send 'Hello'
+        </button>
+        <span>The WebSocket is currently {connectionStatus}</span>
+        {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+        <ul>
+          {messageHistory.map((message, idx) => (
+            <span key={idx}>{message ? message.data : null}</span>
+          ))}
+        </ul>
+      </div> */}
+
       {/* Example using Grid's API */}
       <Button variant="contained" onClick={buttonListener}>
         Push Me
